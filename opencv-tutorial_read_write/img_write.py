b15befd6f08c8c600dd9fddb1a55195d023536cb
JEONGHWAN-SHIN <117723921+KOR-JHSHIN@users.noreply.github.com>
2024-07-03 02:18:54 +0900

    Add files via upload

---
+++ opencv-tutorial_read_write/img_write.py
@@ -0,0 +1,15 @@
+import cv2
+
+img_file_path = './example_img1.jpg'
+save_img_file_path = './gray_example_img1.jpg'
+
+img = cv2.imread(img_file_path, cv2.IMREAD_GRAYSCALE) # image reading in grayscale
+
+if img is not None:                                  # checking if the image is successfully loaded
+    cv2.imshow('IMG', img)                           # showing the image
+    cv2.imwrite(save_img_file_path, img)             # saving the image
+    print("Image saved successfully.")               # printing message indicating the image was saved
+    cv2.waitKey()                                    # waiting for a key press event
+    cv2.destroyAllWindows()                          # closing all windows
+else:
+    print("No image file.")                          # printing error message if image is not loaded
